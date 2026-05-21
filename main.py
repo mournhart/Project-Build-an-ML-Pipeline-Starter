992db192109acf34ebd372aa4d33ce12beb2f344
mournhart <wcshek@SpeedyCrow.localdomain>
2026-05-20 23:06:42 -0700

    Added data spliting to pipeline (in main.py)

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def go(config: DictConfig):
 
         if "basic_cleaning" in active_steps:
             ##################
-            # Implement here #
+            # Parameters for the cleaning step. #
             ##################
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
@@ -69,7 +69,7 @@ def go(config: DictConfig):
 
         if "data_check" in active_steps:
             ##################
-            # Implement here #
+            # Parameters for data check step. #
             ##################
              _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
@@ -85,9 +85,18 @@ def go(config: DictConfig):
 
         if "data_split" in active_steps:
             ##################
-            # Implement here #
+            # Code for splitting the data. #
             ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
