8116a204d71579cd53491506e71c6c2a7034f403
mournhart <wcshek@SpeedyCrow.localdomain>
2026-05-20 22:47:49 -0700

    Updated test_data.py method to check price range

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -92,5 +92,5 @@ def test_row_count(data):
         assert 15000 < data.shape[0] < 1000000
 
 def test_price_range(data, min_price, max_price):
-        assert data['price'].between(min_price, max_price)
+        assert data['price'].between(min_price, max_price).all(), "Note: Some prices are outside the allowed range."
 
